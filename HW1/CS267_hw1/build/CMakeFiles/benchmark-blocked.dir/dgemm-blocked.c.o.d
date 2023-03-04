5ee8bd35ffe1cf783cd25cafbb565eda4a66e87d
kimjoyc <88675769+kimjoyc@users.noreply.github.com>
2023-03-04 05:52:18 -0800

    Delete HW1/CS267_hw1 directory

--- HW1/CS267_hw1/build/CMakeFiles/benchmark-blocked.dir/dgemm-blocked.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/benchmark-blocked.dir/dgemm-blocked.c.o: \
- /home/joyce/hw1/dgemm-blocked.c /usr/include/stdc-predef.h
--- HW1/CS267_hw1/build/CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/benchmark-naive.dir/dgemm-naive.c.o: \
- /home/joyce/hw1/dgemm-naive.c /usr/include/stdc-predef.h

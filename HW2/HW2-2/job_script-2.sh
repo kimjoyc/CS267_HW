b207f85ae3dbe45245f3831da7d84976c75c801a
kimjoyc <88675769+kimjoyc@users.noreply.github.com>
2023-03-06 23:43:05 -0800

    scripts

---
+++ HW2/HW2-2/job_script-2.sh
@@ -0,0 +1,7 @@
+
+
+sbatch job-mpi_20
+sbatch job-mpi_30
+sbatch job-mpi_40
+sbatch job-mpi_50
+sbatch job-mpi_64
---
+++ HW2/HW2-2/job_script.sh
@@ -0,0 +1,13 @@
+# sbatch job-mpi_2_20
+# sbatch job-mpi_2_30
+# sbatch job-mpi_2_40
+# sbatch job-mpi_2_50
+# sbatch job-mpi_2_64
+
+
+
+sbatch job-mpi_20
+sbatch job-mpi_30
+sbatch job-mpi_40
+sbatch job-mpi_50
+sbatch job-mpi_64
